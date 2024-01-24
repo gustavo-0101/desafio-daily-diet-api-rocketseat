eb99119ac6dc9982c9c2f74e7d2c4ec8cb6c78ab
Gustavo Ramisch Dias <gustavo@gramadosummit.com>
2024-01-24 00:46:23 -0300

    Get and update one single meal

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -40,4 +40,65 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.send({ meals })
     },
   )
+
+  app.get(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals')
+        .where({ id, user_id: request.user?.id })
+        .first()
+
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      return reply.send({ meal })
+    },
+  )
+
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals')
+        .where({ id, user_id: request.user?.id })
+        .first()
+
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+      })
+
+      const { name, description, isOnDiet } = createMealBodySchema.parse(
+        request.body,
+      )
+
+      const updatedDate = new Date()
+
+      await knex('meals')
+        .where({ id, user_id: request.user?.id })
+        .update({
+          name,
+          description,
+          is_diet: isOnDiet,
+          updated_at: `${updatedDate.getUTCFullYear()}-${(updatedDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${updatedDate.getUTCDate().toString().padStart(2, '0')} ${updatedDate.getUTCHours().toString().padStart(2, '0')}:${updatedDate.getUTCMinutes().toString().padStart(2, '0')}:${updatedDate.getUTCSeconds().toString().padStart(2, '0')}`,
+        })
+
+      return reply.status(204).send()
+    },
+  )
 }
--- src/test/meals.spec.ts
+++ src/test/meals.spec.ts
@@ -40,9 +40,11 @@ describe('Meals routes', () => {
       .send({ name: 'Test Name', email: 'example@example.com' })
       .expect(201)
 
+    const cookies = userResponse.get('Set-Cookie')
+
     await request(app.server)
       .post('/meals')
-      .set('Cookie', userResponse.get('Set-Cookie'))
+      .set('Cookie', cookies)
       .send({
         name: 'Test meal',
         description: 'Meal created to test',
@@ -50,21 +52,105 @@ describe('Meals routes', () => {
       })
       .expect(201)
 
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(mealsResponse.body.meals).toEqual([
+      expect.objectContaining({
+        name: 'Test meal',
+        description: 'Meal created to test',
+        is_diet: 1, // sqlite returns a 1 as true and 0 as false,
+      }),
+    ])
+  })
+
+  it('should be able to show a single meal', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'Teste', email: 'teste@teste.com' })
+      .expect(201)
+
+    const cookies = userResponse.get('Set-Cookie')
+
     await request(app.server)
       .post('/meals')
-      .set('Cookie', userResponse.get('Set-Cookie'))
+      .set('Cookie', cookies)
       .send({
-        name: 'Test meal 2',
-        description: 'Meal created to test 2',
+        name: 'Teste',
+        description: 'Descrição de teste',
         isOnDiet: true,
       })
       .expect(201)
 
     const mealsResponse = await request(app.server)
       .get('/meals')
-      .set('Cookie', userResponse.get('Set-Cookie'))
+      .set('Cookie', cookies)
       .expect(200)
 
-    expect(mealsResponse.body.meals).toHaveLength(2)
+    const mealId = mealsResponse.body.meals[0].id
+
+    const mealResponse = await request(app.server)
+      .get(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(mealResponse.body).toEqual({
+      meal: expect.objectContaining({
+        name: 'Teste',
+        description: 'Descrição de teste',
+        is_diet: 1,
+      }),
+    })
+  })
+
+  it('should be able to update a meal', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'Teste', email: 'teste@teste.com' })
+      .expect(201)
+
+    const cookies = userResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Teste',
+        description: 'Descrição de teste',
+        isOnDiet: true,
+      })
+      .expect(201)
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const id = mealsResponse.body.meals[0].id
+
+    await request(app.server)
+      .put(`/meals/${id}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'Teste modificação',
+        description: 'Descrição de teste modificação',
+        isOnDiet: false,
+      })
+      .expect(204)
+
+    const mealNewResponse = await request(app.server)
+      .get(`/meals/${id}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(mealNewResponse.body).toEqual({
+      meal: expect.objectContaining({
+        name: 'Teste modificação',
+        description: 'Descrição de teste modificação',
+        is_diet: 0,
+      }),
+    })
   })
 })
