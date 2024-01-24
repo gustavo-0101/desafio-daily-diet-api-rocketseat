1e1113ca850079f43c7711728fb7ba5a63e3e61f
Gustavo Ramisch Dias <gustavo@gramadosummit.com>
2024-01-23 22:02:07 -0300

    Added users migration

--- db/migrations/20240123224320_create-meals.ts
+++ db/migrations/20240123224320_create-meals.ts
@@ -3,11 +3,12 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
-    table.uuid('session_id').after('id').index()
+    table.uuid('user_id').references('users.id').notNullable()
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.boolean('is_diet').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
